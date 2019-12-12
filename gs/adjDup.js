60b4d1a700654a78babf6a485dc0aa18fb9c2757
wieczorekc18 <wieczorekc18@gmail.com>
2019-12-11 19:56:51 -0500

    add change

---
+++ gs/adjDup.js
@@ -0,0 +1,81 @@
+/*
+
+Given a string, recursively remove adjacent duplicate characters from the string. 
+The output string should not have any adjacent duplicates. 
+
+See following examples.
+
+Examples:
+
+Input: azxxzy
+Output: ay
+First “azxxzy” is reduced to “azzy”.
+The string “azzy” contains duplicates,
+so it is further reduced to “ay”.
+
+*/
+
+
+
+
+// not completed
+
+function removeAdjDup(string){
+    if(string.length <= 1) return string;
+    let start = 0;
+    let end = 0;
+    while(string[start] == string[end]){
+        end++;
+    }
+    let newString = string.slice(end)
+    let oldString;
+    if(end > 1){
+        oldString = "";
+    }else{
+        oldString = string.slice(start, end)
+    }
+    return oldString + removeAdjDup(newString)
+}
+
+function anyDups(string){
+    for(let i = 0; i < string.length; i++){
+        if(string[i] === string[i+1]){
+            return true
+        }
+    }
+    return false
+}
+
+function remove(string){
+    while(anyDups(string)){
+        string = removeAdjDup(string)
+    }
+    return string
+}
+
+string1 = "geeksforgeeg"
+console.log(remove(string1))
+
+string2 = "azxxxzy"
+console.log(remove(string2))
+
+string3 = "caaabbbaac"
+console.log(remove(string3))
+
+string4 = "gghhg"
+console.log(remove(string4))
+
+string5 = "aaaacddddcappp"
+console.log(remove(string5))
+
+string6 = "aaaaaaaaaa"
+console.log(remove(string6))
+
+string7 = "qpaaaaadaaaaadprq"
+console.log(remove(string7))
+
+string8 = "acaaabbbacdddd"
+console.log(remove(string8))
+
+string9 = "acbbcddc"
+console.log(remove(string9))
--- gs/minEle.js
+++ gs/minEle.js
@@ -1,5 +1,5 @@
 
-function minEle(arr){
+function minimumEle(arr){
     if(arr.length == 0) return;
     let mid = Math.floor(arr.length/2)
     let last = arr[arr.length-1];
@@ -12,6 +12,18 @@ function minEle(arr){
     }
 
     
+}
+
+function minEle(array){
+    if(array.length <=1) return array[0]
+    let mid = Math.floor(array.length/2)
+    if(array[mid] > array[mid+1]) return array[mid+1];
+    if (array[mid-1] > array[mid]) return array[mid]
+    if(array[mid] > array[array.length -1]){
+        return minEle(arr.slice(0, mid))
+    } else {
+        return minEle(arr.slice(mid + 1))
+    }
 }
 
 let arr = [5, 6, 2, 3, 4]
